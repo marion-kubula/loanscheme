368243d3f8445ebc42cdc631baa34142addc8959
marion <kmarion548@gmail.com>
2021-09-13 21:31:09 +0300

    my first commit

---
+++ loan.js
@@ -0,0 +1,33 @@
+let submitEl=document.getElementById('submit')
+submitEl.addEventListener('click',()=>{
+    
+})
+let loanRepayment=[
+    
+    { 
+        id:0,
+        name:firstinstallment,
+        value:2000000,
+        d:new Date()
+        
+    },
+    {
+        id:0,
+        name:secondinstallment,
+        value:1000000,   
+    },
+    {
+        id:0,
+        installment,
+        name:thirdinstallment,
+        value:500000,
+    }
+]
+const admin;
+function verify(){
+    if(application.user.address){
+        console.log('application verified')
+    }
+}
+const verifyEl=document.getElementById('verify')
+verifyEl.innerHtml=verify;
