ea4f12bd90b6f6842702bed08d07701d3a426af5
marion <kmarion548@gmail.com>
2021-11-18 00:34:52 +0300

    add login page

--- loan.js
+++ loan.js
@@ -1,29 +1,32 @@
 let submitEl=document.getElementById('submit')
 submitEl.addEventListener('click',()=>{
+
     
 })
 let loanRepayment=[
     
     { 
         id:0,
-        name:firstinstallment,
+        name:'firstinstallment',
         value:2000000,
         d:new Date()
         
     },
     {
         id:0,
-        name:secondinstallment,
+        name:'secondinstallment',
         value:1000000,   
     },
     {
         id:0,
-        installment,
-        name:thirdinstallment,
+        name:'thirdinstallment',
         value:500000,
     }
 ]
-const admin;
+const payement= document.getElementsById('admin');
+
+
+const admin=document.getElementById('adimin');
 function verify(){
     if(application.user.address){
         console.log('application verified')
@@ -31,3 +34,12 @@ function verify(){
 }
 const verifyEl=document.getElementById('verify')
 verifyEl.innerHtml=verify;
+
+const apply= document.getElementsByClassName(apply);
+const loanapplication =1;
+if( loanapplication== 1){
+
+    alert("loan application accepted");
+}else{
+    alert("you can only apply once");
+}
